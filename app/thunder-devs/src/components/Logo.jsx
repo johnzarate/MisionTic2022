5802ba86513bd7a38f4f77ad9926777e4d13cee4
Sarha Melissa Chica Otalvaro <sarhachica@gmail.com>
2021-10-25 23:42:09 -0500

    Cerrar Sesión

--- app/thunder-devs/src/components/Logo.jsx
+++ app/thunder-devs/src/components/Logo.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import image from 'media/logo.png';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const Logo = () => {
+  const {user, isAuthenticated} = useAuth0();
   return (
       <>
-        <img className='mx-auto h-40 w-auto mt-8 mb-4' src={image} alt='App Icon' />
+        <img className='mx-auto h-40 w-auto mt-8 mb-4' src={isAuthenticated ? user.picture: image} alt='App Icon' />
         <hr className="bg-black block w-full h-0.5 mb-8 opacity-75"/>
       </>
   )
--- app/thunder-devs/src/components/admin/AdminSideBar.jsx
+++ app/thunder-devs/src/components/admin/AdminSideBar.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../Logo';
 import OptionSideBar from '../OptionSideBar';
+import { useAuth0 } from '@auth0/auth0-react';
+
 
 
 const AdminSideBar = () => {
+    const {logout} = useAuth0();
 
     return (
         <nav className="hidden lg:flex lg:w-72 border border-gray-300 h-full flex-col bg-gray-200 p-2">
@@ -23,7 +26,14 @@ const AdminSideBar = () => {
 
                 <OptionSideBar icon="account_circle" route="/dd" label="Profile" />
 
-                <OptionSideBar icon="account_circle" route="/" label="Profile" mod="bottom-0"/>
+                <button
+                className={`p-1 mb-2 bg-green-500 hover:bg-green-300 hover:scale-150 flex w-full items-center text-white rounded-lg shadow-lg `}
+            onClick={logout}>
+                <p className="flex items-center m-2 text-center">
+                    <span className="material-icons mr-5">
+                    </span> Cerrar Sesión
+                </p>
+            </button>
 
             </div>
 
